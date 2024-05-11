775143d35c8ac05fcaa6b5e0d52dc38c70df99b7
stefan1anuby <stefaneduard2002@gmail.com>
2024-05-11 17:34:42 +0300

    added firestore to the project

--- lib/src/auth.dart
+++ lib/src/auth.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   User? get currentUser => _firebaseAuth.currentUser;
-
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
   Future<void> signInWithEmailAndPassword(
@@ -15,8 +16,18 @@ class Auth {
 
   Future<void> createUserWithEmailAndPassword(
       {required String email, required String password}) async {
-    await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
+    UserCredential userCredential = await _firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    User? user = userCredential.user;
+    if (user != null) {
+      // Crează un document pentru noul utilizator în Firestore
+      await _firestore.collection('Utilizatori').doc(user.uid).set({
+        'email': user.email,
+        'puncte': 0, // Valoare inițială pentru puncte
+        'data_ultimei_donari': null,
+        'istoric_donari': [],
+      });
+    }
   }
 
   Future<void> signOut() async {
