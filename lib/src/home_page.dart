d4237a34b17849c3aad614ce968365575d665e93
stefan1anuby <stefaneduard2002@gmail.com>
2024-05-11 17:30:37 +0300

    added bottomnavbar

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,18 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:blooddochallenge/src/auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
-  final User? user = Auth().currentUser;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final User? user = FirebaseAuth.instance.currentUser;
 
   Future<void> signOut() async {
-    await Auth().signOut();
+    await FirebaseAuth.instance.signOut();
   }
 
   Widget _title() {
-    return const Text('Firebase Auth');
+    return const Text('BloodDoChallenge');
   }
 
   Widget _userUid() {
@@ -23,21 +27,47 @@ class HomePage extends StatelessWidget {
     return ElevatedButton(onPressed: signOut, child: const Text('Sign Out'));
   }
 
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: Home', style: optionStyle),
+    Text('Index 1: Ranking', style: optionStyle),
+    Text('Index 2: History', style: optionStyle),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: _title(),
-          backgroundColor: Colors.blue,
-        ),
-        body: Container(
-            height: double.infinity,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[_userUid(), _signOutButton()],
-            )));
+      appBar: AppBar(
+        title: _title(),
+        backgroundColor: Colors.blue,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: signOut,
+          ),
+        ],
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Ranking'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'History'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
